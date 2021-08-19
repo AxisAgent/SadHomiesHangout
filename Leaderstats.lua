5480213e427df0ca36f9124deb7bc4a3cdee7590
AxisAgent <77366347+AxisAgent@users.noreply.github.com>
2021-08-18 17:00:34 -0700

    only lvl now

--- Leaderstats.lua
+++ Leaderstats.lua
@@ -1,31 +1,20 @@
 local Players = game:GetService("Players")
 local DataStoreService = game:GetService("DataStoreService")
 local LevelAndXpStore1 = DataStoreService:GetDataStore("LevelAndXpStore1")
-local LevelAndXpStore2 = DataStoreService:GetDataStore("LevelAndXpStore2")
-local StartValLvl = 1
-local StartValXp = 30
 local function leaderboardSetup(player)
+	local data1 = LevelAndXpStore1:GetAsync(player.UserId)
 	local leaderstats = Instance.new("Folder")
 	leaderstats.Name = "leaderstats"
 	leaderstats.Parent = player
 	
-	local hs = Instance.new("Folder")
-	hs.Name = "hiddenstats"
-	hs.Parent = player
 
 	local lvl = Instance.new("IntValue")
 	lvl.Name = "Level"
-	lvl.Value = LevelAndXpStore1:GetAsync(player.UserId)
+	lvl.Value = data1
 	lvl.Parent = leaderstats
-	
-	local xpl = Instance.new("IntValue")
-	xpl.Name = "curxp"
-	xpl.Value = LevelAndXpStore2:GetAsync(player.UserId)
-	xpl.Parent = hs
-	local xpl2 = Instance.new("IntValue")
-	xpl2.Name = "xpneed"
-	xpl2.Value = 30 * lvl.Value
-	xpl2.Parent = hs
+	if not data1 then
+		lvl.Value = 1
+	end
 end
 
 -- Connect the "leaderboardSetup()" function to the "PlayerAdded" event
@@ -33,5 +22,4 @@ Players.PlayerAdded:Connect(leaderboardSetup)
 
 Players.PlayerRemoving:Connect(function(player)
 	LevelAndXpStore1:SetAsync(player.UserId,player.leaderstats.Level.Value)
-	LevelAndXpStore2:SetAsync(player.UserId,player.hiddenstats.curxp.Value)
 end)
